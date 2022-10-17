7ca219f68611b0456bc58865d8832cf28d3cfe39
overol1221 <fiqiachmad12@gmail.com>
2022-10-17 20:28:56 +0700

    Pivot menggunakan quick sort mengurutkan daftar pivot menggunakan quick sort

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+using System.Xml;
 
 namespace QuickShort
 {
@@ -10,41 +9,41 @@ namespace QuickShort
     {
         //array of integers to hold values
         private int[] arr = new int[20];
-        private int cmp_count = 0; // number of comparasion
-        private int mov_count = 0; // number of data movements
+        private int cmp_count = 0;
+        private int mov_count = 0;
 
-        // Number of elements in array
+        //Number of elements in array
         private int n;
 
         void read()
         {
             while (true)
             {
-                Console.Write(" Enter the number of elements in the array :");
+                Console.WriteLine("Enter the number of elements in the array :");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if (n <= 20)
                     break;
                 else
-                    Console.WriteLine("\nArray can have maximum 20 element \n");
+                    Console.WriteLine("\n Array can have maximum 20 elements \n");
             }
-            Console.WriteLine("\n=====================");
+            Console.WriteLine("\n====================");
             Console.WriteLine("Enter Array Elements");
-            Console.WriteLine("\n=====================");
+            Console.WriteLine("\n====================");
 
             //get array elements
             for (int i = 0; i < n; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
-                arr[1] = Int32.Parse(s1);
+                arr[i] = Int32.Parse(s1);
             }
+
         }
-        // swaps the element at index x with the element at index y
+        //swaps the element at index x with the element at index y
         void swap(int x, int y)
         {
             int temp;
-
             temp = arr[x];
             arr[x] = arr[y];
             arr[y] = temp;
@@ -55,9 +54,10 @@ namespace QuickShort
             int pivot, i, j;
             if (low > high)
                 return;
-            //Parition the list into two parts;
-            //one countaining elements less that or equal to pivot
-            //Outher countaining elements greather than pivot
+
+            //Partition the list into two parts
+            //One containing elements less that or equal to pivot
+            //Outher containing elements greater than pivot
 
             i = low + 1;
             j = high;
@@ -66,17 +66,40 @@ namespace QuickShort
 
             while (i <= j)
             {
-                //Search for an element greater than pivot
-                while ((arr[i] <= pivot) && (i <= high))
+                //search for an element greater than pivot
+                while ((arr[i]) <= pivot && (i <= high))
                 {
                     i++;
                     cmp_count++;
                 }
                 cmp_count++;
 
-                if (i < j) //If the greator element is on the left of the element
+                //search for an element less than or equal to pivot
+                while ((arr[j]) > pivot && (j >= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) //if the greater element is on the left of the element
                 {
                     //swap the element at index i with the element at index j
                     swap(i, j);
                     mov_count++;
                 }
+            }
+            // j now contains the index of the last elements int the sorted list
+
+            if (low < j)
+            {
+                //Move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
+        }
