8706ef7a681abd14ce3a3ad5247835929744e587
overol1221 <fiqiachmad12@gmail.com>
2022-10-17 19:39:41 +0700

    Menambahkan nomor di element array dan membuat maksimal 20.

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -15,4 +15,16 @@ namespace QuickShort
 
         // Number of elements in array
         private int n;
+
+        void read()
+        {
+            while (true)
+            {
+                Console.Write(" Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 element \n");
      
